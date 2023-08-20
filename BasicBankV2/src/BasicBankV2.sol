f91587e983af63b9e9a6dfa570f241c6b80efa10
ssghost <ssprof0@gmail.com>
2023-08-21 00:04:03 +0800

    finish basicbankv2

--- BasicBankV2/src/BasicBankV2.sol
+++ BasicBankV2/src/BasicBankV2.sol
@@ -8,9 +8,12 @@ contract BasicBankV2 {
 
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {payable(address(this)).transfer(msg.value);
+                                          balances[msg.sender] += msg.value;}
 
     /// @notice used to withdraw ether from the contract
     /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {(bool s,) = payable(msg.sender).call{value:amount}("");
+                                                           require(s);
+                                                           balances[msg.sender] -= amount;}
 }
