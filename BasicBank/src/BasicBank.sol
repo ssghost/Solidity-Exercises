1ca3f7bd45d09ccf3a898f466c73d142a5e7e377
ssghost <ssprof0@gmail.com>
2023-08-20 23:51:50 +0800

    finish basicbank

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.13;
 contract BasicBank {
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {payable(address(this)).transfer(msg.value);}
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {(bool s,) = payable(msg.sender).call{value:amount}("");require(s);}
 }
